1d494734233bb5183c3631cdba5deaecd1828749
Carlos23 <carlos.enriquez.valerio@gmail.com>
2020-09-12 21:57:57 -0500

    Grid Test

---
+++ HispanicJamProyect/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+   [SerializeField] Grid grid;
+   // Start is called before the first frame update
+   void Start()
+   {
+
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      if (Input.GetKeyDown(KeyCode.D))
+      {
+         Vector3Int positionInGrid = grid.WorldToCell(transform.position);
+         positionInGrid += new Vector3Int(1, 0, 0);
+         transform.position = grid.CellToWorld(positionInGrid);
+      }
+   }
+}
